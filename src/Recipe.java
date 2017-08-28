45ae59d53e087f9e99bd28203d5eb8e1e8c92d13
Yavax <yavax.dev@gmail.com>
2017-08-28 08:40:27 +0200

    Add variables and methods to Recipe class

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,4 +1,56 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Recipe {
+public class Recipe extends ProductRecipe{
+	
+	protected ArrayList<ProductRecipe> productsList = new ArrayList<ProductRecipe>();
+	protected String name;
+	protected String fullRecipe;
+	
+	public Recipe(String name,
+				String fullRecipe)
+	{
+		this.name = name;
+		this.fullRecipe = fullRecipe;
+	}
+	
+	@Override
+	public void add(ProductRecipe product) {
+		this.productsList.add(product);
+	}
+	
+	@Override
+	public void remove(ProductRecipe product) {
+		this.productsList.remove(product);
+	}
+	
+	@Override
+	public ProductRecipe getChild(int i) {
+		return (ProductRecipe)productsList.get(i);
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getFullRecipe() {
+		return fullRecipe;
+	}
+	
+	@Override
+	public void print() {
+		System.out.println("Nazwa: " + name);
+		System.out.println("Przepis:" + fullRecipe);
+		System.out.println("-----------------------");
+		
+		Iterator<ProductRecipe> iterator = productsList.iterator();
+		while(iterator.hasNext()) {
+			ProductRecipe productRecipe = (ProductRecipe) iterator.next();
+			productRecipe.print();
+		}
+		
+	}
 
 }
