8b56f8663498b9361093c8c083474c9ee9737f06
Yavax <yavax.dev@gmail.com>
2017-08-28 09:12:34 +0200

    Add method to MasterChef

--- src/MasterChef.java
+++ src/MasterChef.java
@@ -1,4 +1,14 @@
 
 public class MasterChef {
+	
+	protected ProductRecipe allRecipes;
+	
+	public MasterChef(ProductRecipe allRecipes) {
+		this.allRecipes = allRecipes;
+	}
+	
+	public void printAllRecipes() {
+		allRecipes.print();
+	}
 
 }
