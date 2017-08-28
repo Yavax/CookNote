f6d6a8c412f47a7e291c9e908c1b1744aec6c57f
Yavax <yavax.dev@gmail.com>
2017-08-28 08:17:37 +0200

    Add variables and methods to Product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,39 @@
 
 public class Product extends ProductRecipe{
 
+	protected String name;
+	protected String quantity;
+	
+	public Product(String name,
+				String quantity)
+	{
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getQuantity() {
+		return quantity;
+	}
+	
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public void print() {
+		System.out.println(" *" + name + " - " + quantity);
+	}
+	
 }
