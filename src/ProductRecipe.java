d5c125acfe8ece668d34d7734c87d90dfbcc95e3
Yavax <yavax.dev@gmail.com>
2017-08-28 07:52:10 +0200

    Create abstract class ProductRecipe

--- src/ProductRecipe.java
+++ src/ProductRecipe.java
@@ -1,5 +1,44 @@
 
 public abstract class ProductRecipe {
 	
+	public void add(ProductRecipe productRecipe) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(ProductRecipe productRecipe) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void setName(String name) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void setFullRecipe(String recipe) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void setQuantity(String quantity) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void getChild(int i) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getName() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getFullRecipe() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getQuantity() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void print() {
+		throw new UnsupportedOperationException();
+	}
 
 }
