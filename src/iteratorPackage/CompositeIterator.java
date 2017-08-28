609e795bad7936bffa1ba427b23c320b4cf3c62d
Yavax <yavax.dev@gmail.com>
2017-08-28 14:38:33 +0200

    Create next, hasNext, remove to CompositeIterator

--- src/iteratorPackage/CompositeIterator.java
+++ src/iteratorPackage/CompositeIterator.java
@@ -1,13 +1,43 @@
 package iteratorPackage;
 
 import java.util.*;
+import masterChef.*;
 
-public class CompositeIterator implements Iterator{
+public class CompositeIterator implements Iterator {
 	Stack stack = new Stack();
 	
 	public CompositeIterator(Iterator iterator) {
 		stack.push(iterator);
 	}
 	
+	public Object next() {
+		if(hasNext()) {
+			Iterator iterator = (Iterator) stack.peek();
+			ProductRecipe product = (ProductRecipe) iterator.next();
+			if(product instanceof ProductRecipe) {
+				stack.push(product.createIterator());
+			}
+			return product;
+		}else {
+			return null;
+		}
+	}
+	
+	public boolean hasNext() {
+		if(stack.empty()) {
+			return false;
+		}else {
+			Iterator iterator = (Iterator) stack.peek();
+			if(!iterator.hasNext()) {
+				stack.pop();
+				return hasNext();
+			}else
+				return true;
+		}
+	}
+	
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
 
 }
