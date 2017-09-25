0aa4252d73a958fc9906eb705a1e9237997f28d5
Yavax <yavax.dev@gmail.com>
2017-09-25 21:01:33 +0200

    Create mainWindow class

---
+++ src/window/mainWindow.java
@@ -0,0 +1,13 @@
+package window;
+
+import javax.swing.*;
+
+public class mainWindow extends JFrame {
+	
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 200;
+	
+	public void MyFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
